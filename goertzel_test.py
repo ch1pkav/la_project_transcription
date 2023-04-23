ca78eac6f00cb3731ce6d11ae2c1b8a7c8c3cf38
Teodor Muzychuk <teodor.muzychuk@ucu.edu.ua>
2023-04-23 16:37:51 +0300

    working midi conversion

--- goertzel_test.py
+++ goertzel_test.py
@@ -4,6 +4,8 @@ import math
 import wave
 import librosa
 import numpy as np
+from midiutil import MIDIFile
+
 
 def goertzel(samples, sample_rate, *freqs):
     """
@@ -29,7 +31,8 @@ def goertzel(samples, sample_rate, *freqs):
         k_start = int(math.floor(f_start / f_step))
         k_end = int(math.ceil(f_end / f_step))
 
-        if k_end > window_size - 1: raise ValueError('frequency out of range %s' % k_end)
+        if k_end > window_size - 1:
+            raise ValueError('frequency out of range %s' % k_end)
         bins = bins.union(range(k_start, k_end))
 
     # For all the bins, calculate the DFT term
@@ -46,7 +49,7 @@ def goertzel(samples, sample_rate, *freqs):
         # Doing the calculation on the whole sample
         d1, d2 = 0.0, 0.0
         for n in n_range:
-            y  = samples[n] + w_real * d1 - d2
+            y = samples[n] + w_real * d1 - d2
             d2, d1 = d1, y
 
         # Storing results `(real part, imag part, power)`
@@ -57,21 +60,44 @@ def goertzel(samples, sample_rate, *freqs):
         freqs.append(f * sample_rate)
     return freqs, results
 
+
 if __name__ == '__main__':
 
     frames, sr = librosa.load('untitled.wav', sr=None)
-    c_major_freqs = [(250, 270), (285, 298), (325, 335), (345, 355), (380, 400), (430, 450), (480, 500)]
+    bpm = librosa.beat.tempo(y=frames, sr=sr)[0]
+    c_major_freqs = [(250, 270), (288, 298), (320, 340),
+                     (345, 355), (380, 400), (430, 450), (480, 500)]
+    c_major_freqs += [(x[0] * 2, x[1] * 2) for x in c_major_freqs]
     onsets = librosa.onset.onset_detect(y=frames, sr=sr, units='samples')
     notes = []
     for index, onset in enumerate(onsets):
         if index == len(onsets) - 1:
             freqs, results = goertzel(frames[onset:], sr, *c_major_freqs)
         else:
-            freqs, results = goertzel(frames[onset:onsets[index+1]], sr, *c_major_freqs)
+            freqs, results = goertzel(
+                frames[onset:onsets[index+1]], sr, *c_major_freqs)
         # print(freqs[results.index(max(results, key=lambda x: x[2]))])
+        # if index == 1 or index == 2:
+            # plt.plot(freqs, [x[2] for x in results])
+            # plt.show()
         notes.append(freqs[results.index(max(results, key=lambda x: x[2]))])
 
-    print(notes)
-    with plt.xkcd():
-        plt.plot(onsets, notes, linestyle='None', marker='o')
-        plt.show()
+    # with plt.xkcd():
+
+    midi = MIDIFile(1)
+    quarter_note = 60 / bpm
+    midi.addTempo(0, 0, bpm)
+    onsets = [x / quarter_note / sr for x in onsets]
+    offsets = [x for x in onsets[1:]] + [onsets[-1] + 1]
+    durations = [x - y for x, y in zip(offsets, onsets)]
+    for index, note in enumerate(notes):
+:x
+
+        midi.addNote(0, 0, int(librosa.hz_to_midi(note)),
+                     onsets[index], durations[index], 100)
+
+    with open("untitled.mid", "wb") as output_file:
+        midi.writeFile(output_file)
+
+    plt.plot(onsets, notes, linestyle='None', marker='o')
+    plt.show()
